c5ce2459b9a3c8c93bb77f40bbcbdbc22379eec0
amilasm <amilasampath1400@gmail.com>
2022-09-03 19:21:55 +0530

    supplier interfaces

---
+++ src/Views/Supplier/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container is-max-desktop">
+          
+      <header class="card-header">
+        <div class="card-header-title">
+            <div class="card-header-title">
+             <div style="font-size: larger;"> Supplier Management</div>
+            </div>
+          <div class="float-end">
+            <b-button  type="is-success" >Add Item</b-button>
+            <b-button  type="is-info" >Generate Report</b-button>
+          </div>
+        </div>
+      </header>
+  
+      <div style="margin-left: 86%">
+  
+      </div>
+      <section>
+        <div class="card mt-5">
+          <div class="card-content">
+            <div class="content">
+              
+            </div>
+          </div>
+        </div>
+  
+  
+      </section>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+      name: "supplier_home"
+      
+    }
+    </script>
+  
+  <style scoped>
+  
+  </style>
+  
--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import auth_router from "./modules/auth_router";
 import admin_router from "./modules/admin-router";
+import supplier_router from "./modules/supplier-router";
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     ...auth_router,
-    ...admin_router
+    ...admin_router,
+    ...supplier_router
   ]
 })
