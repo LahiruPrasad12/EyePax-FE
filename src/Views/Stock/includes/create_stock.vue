954dc1c47dfc4065378b824d9780b6852701229e
Kavindu Lakshan <75882836+kavindu-lakshan@users.noreply.github.com>
2022-09-08 01:03:07 +0530

    do changes

---
+++ src/Views/Stock/includes/create_stock.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "create_stock"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/Views/Stock/includes/edit_stock.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "edit_stock"
+}
+</script>
+
+<style scoped>
+
+</style>
