f57db7ab0238681634fb5dc99d7450856efa02a6
kavindushehan <it20076566@my.sliit.lk>
2022-09-08 11:09:55 +0530

    Added edit order modal

--- src/apis/modules/customer_apis/customer_apis.js
+++ src/apis/modules/customer_apis/customer_apis.js
@@ -4,6 +4,7 @@ const baseURL = '/api/v1'
 export default {
     getAllOrders: () => api.get(`${baseURL}/order/getOrder`),
     createOrder: (payload) => api.post(`${baseURL}/order/addOrder`, payload),
+    updateOrder: (payload) => api.post(`${baseURL}/order/updateOrder`, payload)
     //   updateStaff: (id,payload) => api.put(`${baseURL}/admin/staff/${id}`,payload),
     //   deleteStaff: (id) => api.delete(`${baseURL}/admin/staff/${id}`)
 
