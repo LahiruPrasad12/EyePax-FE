c16025caef16038a83d655f4ac82c8eb83dd4fea
LahiruPrasad12 <lahirupr471@gmail.com>
2022-09-20 22:50:23 +0530

    changes

---
+++ src/Views/Admin/Tracking/includes/all_shipping_items.vue
@@ -0,0 +1,239 @@
+<template>
+
+  <div>
+    <div class="anim" style="--delay:0.2s;">
+      <b-table
+        ref="staffTable"
+        :data="staff"
+        :loading="is_table_loading"
+        hover
+        responsive
+      >
+        <b-table-column field="id" label="ID">
+          <template v-slot:header="{ column }">
+            <b-tooltip :label="column.label" append-to-body dashed>
+              {{ column.label }}
+            </b-tooltip>
+          </template>
+          <template v-slot="props">
+            {{ props.row.id }}
+          </template>
+        </b-table-column>
+        <b-table-column field="full_name" label="Full Name">
+          <template v-slot:header="{ column }">
+            <b-tooltip :label="column.label" append-to-body dashed>
+              {{ column.label }}
+            </b-tooltip>
+          </template>
+          <template v-slot="props">
+            {{ props.row.first_name }} {{ props.row.last_name }}
+          </template>
+        </b-table-column>
+        <b-table-column field="email" label="Email">
+          <template v-slot:header="{ column }">
+            <b-tooltip :label="column.label" append-to-body dashed>
+              {{ column.label }}
+            </b-tooltip>
+          </template>
+          <template v-slot="props">
+            {{ props.row.email }}
+          </template>
+        </b-table-column>
+        <b-table-column field="account_type" label="Role">
+          <template v-slot:header="{ column }">
+            <b-tooltip :label="column.label" append-to-body dashed>
+              {{ column.label }}
+            </b-tooltip>
+          </template>
+          <template v-slot="props">
+            {{ props.row.account_type }}
+          </template>
+        </b-table-column>
+        <b-table-column field="DOB" label="DOB">
+          <template v-slot:header="{ column }">
+            <b-tooltip :label="column.label" append-to-body dashed>
+              {{ column.label }}
+            </b-tooltip>
+          </template>
+          <template v-slot="props">
+            {{ props.row.DOB }}
+          </template>
+        </b-table-column>
+        <b-table-column field="mobile" label="Mobile">
+          <template v-slot:header="{ column }">
+            <b-tooltip :label="column.label" append-to-body dashed>
+              {{ column.label }}
+            </b-tooltip>
+          </template>
+          <template v-slot="props">
+            {{ props.row.mobile }}
+          </template>
+        </b-table-column>
+
+        <b-table-column field="action" label="Edit">
+          <template v-slot="props">
+            <b-tooltip label="Edit"
+                       position="is-right" target="">
+              <b-button outlined style="border: hidden; background-color: #1f1d2b;" @click="editStaff(props.row)">
+                <svg class="bi bi-pencil-square" fill="currentColor" height="16" viewBox="0 0 16 16" style="color: #ff7551"
+                     width="16" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                  <path
+                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
+                    fill-rule="evenodd"/>
+                </svg>
+              </b-button>
+            </b-tooltip>
+          </template>
+        </b-table-column>
+
+        <b-table-column field="action" label="Delete">
+          <template v-slot="props">
+            <b-tooltip label="Delete"
+                       position="is-right" target="">
+              <b-button outlined style="border: hidden; background-color: #1f1d2b;" @click="confirmCustomDelete(props.row)">
+                <svg class="bi bi-trash-fill" fill="red" height="16" viewBox="0 0 16 16" width="16" style="color: rgb(252, 72, 72)"
+                     xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                </svg>
+              </b-button>
+            </b-tooltip>
+          </template>
+        </b-table-column>
+      </b-table>
+    </div>
+
+    <create_staff ref="create_staff" @getAllStaff="getAllStaff"/>
+    <edit_staff ref="edit_staff" @getAllStaff="getAllStaff"/>
+    <!--    <createNotice ref="create_form"/>-->
+    <!--    <editeNotice ref="edit_form"/>-->
+  </div>
+</template>
+
+<script>
+import staffApis from '../../../../apis/modules/admin_pais/staff_apis'
+import create_staff from "../../Staff/includes/create_staff";
+import edit_staff from "../../Staff/includes/edit_staff";
+import ToastMixin from "../../../../mixins/ToastMixin";
+import staff_apis from "../../../../apis/modules/admin_pais/staff_apis";
+
+export default {
+  name: "all_shipping_items",
+  mixins:[ToastMixin],
+  components: {
+    create_staff,
+    edit_staff
+  },
+  data() {
+    return {
+      selected_role: undefined,
+      fields: [
+        {
+          field: 'id',
+          label: 'ID',
+          width: '40',
+          numeric: true
+        },
+        {
+          field: 'full_name',
+          label: 'Full Name',
+        },
+        {
+          field: 'email',
+          label: 'Email',
+        },
+        {
+          field: 'Role',
+          label: 'account_type',
+        },
+        {
+          field: 'DOB',
+          label: 'Date OF Birth',
+        },
+        {
+          field: 'mobile',
+          label: 'Mobile',
+        },
+        {
+          field: 'role',
+          label: 'Role',
+        },
+      ],
+      staff: [],
+      is_table_loading: false
+    }
+  },
+  watch: {
+    selected_role() {
+      this.getAllStaff(this.selected_role)
+    }
+  },
+  methods: {
+    async getAllStaff(selected_role) {
+      try {
+        this.is_table_loading = true
+        let respond = (await staffApis.getAllStaff(selected_role)).data.data.users
+        this.staff = respond.map((e, index) => ({
+          id: index + 1,
+          _id:e._id,
+          first_name: e.first_name,
+          last_name: e.last_name,
+          email: e.email,
+          DOB: e.DOB.substring(0, 10),
+          mobile: e.mobile,
+          account_type: e.account_type,
+          gender: e.gender,
+          is_email_verified: e.is_email_verified,
+          is_phone_verified: e.is_phone_verified
+        }))
+      } catch (e) {
+
+      }
+      this.is_table_loading = false
+    },
+
+    closeModel() {
+      this.getAllStaff(this.selected_role)
+    },
+
+    editStaff(data) {
+      try {
+        this.$refs.edit_staff.openModal(data)
+      } catch (e) {
+
+      }
+    },
+
+    confirmCustomDelete(data) {
+      this.$buefy.dialog.confirm({
+        title: 'Deleting Account',
+        message: 'Are you sure you want to <b style="color:white;">Delete</b> your account? This action cannot be undone.',
+        confirmText: 'Delete Account',
+        type: 'is-danger',
+        hasIcon: true,
+        onConfirm: () => this.deleteStaff(data)
+      })
+    },
+    async deleteStaff(data) {
+      try{
+
+        await staff_apis.deleteStaff(data._id)
+        this.success('Staff Update Successfully')
+        await this.getAllStaff(this.selected_role)
+      }catch (e) {
+        this.$buefy.toast.open(e.message)
+      }
+    }
+  },
+
+  async mounted() {
+    await this.getAllStaff(this.selected_role)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/Views/Admin/Tracking/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="main-container" style="width: 1000px;">
+      <div class="anim" style="--delay:0.2s;">
+        <h1 style="text-align:center; font-size: 30px; color: white; margin-top: 2%; font-weight: 600;">Tracking
+          Items</h1>
+        <br/><br/>
+      </div>
+      <div class="anim" style="--delay:0.2s;">
+        <header class="card-header" style="background-color: rgba(21, 20, 26, 0.63); border-radius: 20px">
+          <div class="card-header-title">
+            <div class="card-header-title">
+              <div style="font-size: 25px; color: white;">Items</div>
+              <div class="search-bar">
+                <input placeholder="Search Items" style="margin-left:25%;" type="text">
+              </div>
+              <b-select v-model="selected_role" placeholder="Select a Role" style="margin-left: 20%;">
+                <option style="color: rgb(180, 180, 180);" value="">Select role</option>
+                <option style="color: rgb(180, 180, 180);" value="admin">Admin</option>
+                <option style="color: rgb(180, 180, 180);" value="staff">Staff</option>
+                <option style="color: rgb(180, 180, 180);" value="supplier">Supplier</option>
+                <option style="color: rgb(180, 180, 180);" value="stock-manager">Stock Manager</option>
+              </b-select>
+            </div>
+          </div>
+
+        </header>
+      </div>
+      <br/>
+
+      <All_Items ref="all_items"/>
+
+      <div class="ft anim" style="--delay:0.3s; margin-top: 5%;">
+        <p style="text-align:center">
+          © 2022. EyePax Technologies. All Rights Reserved.
+        </p>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+import All_Items from "./includes/all_shipping_items";
+
+export default {
+  name: "index",
+  components: {All_Items},
+  data() {
+    return {
+      selected_role: ''
+    }
+  },
+  watch: {
+    selected_role() {
+      this.filterUser(this.selected_role)
+    }
+  },
+  methods: {
+    createStaff() {
+      this.$refs.all_items.$refs.create_staff.openModal()
+    },
+
+    filterUser(role) {
+      this.$refs.all_items.getAllStaff(role)
+    }
+  }
+}
+</script>
+
+<style scoped>
+#btn {
+  float: right;
+}
+
+</style>
