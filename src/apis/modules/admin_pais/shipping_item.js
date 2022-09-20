dd76aacf524b9a244b4b3e79ea3a4ff5a5dbbefa
LahiruPrasad12 <lahirupr471@gmail.com>
2022-09-20 22:53:35 +0530

    created apis for shipping item

---
+++ src/apis/modules/admin_pais/shipping_item.js
@@ -0,0 +1,8 @@
+import api from "../../axios";
+const baseURL = '/api/v1'
+
+export default {
+  getAllShippingItems: (status) => api.get(`${baseURL}/shipping-item${status?`?status=${status}`:''}`),
+  UpdateShippingItems: (id,payload) => api.put(`${baseURL}/shipping-item/${id}`,payload),
+
+}
