ecc651148a9d41144a11644b4f551444e71f0e17
kavindushehan <it20076566@my.sliit.lk>
2022-10-13 00:26:18 +0530

    Delete function done

--- src/apis/modules/customer_apis/customer_apis.js
+++ src/apis/modules/customer_apis/customer_apis.js
@@ -4,8 +4,6 @@ const baseURL = '/api/v1'
 export default {
     getAllOrders: () => api.get(`${baseURL}/order/getOrder`),
     createOrder: (payload) => api.post(`${baseURL}/order/addOrder`, payload),
-    updateOrder: (payload) => api.post(`${baseURL}/order/updateOrder`, payload)
-    //   updateStaff: (id,payload) => api.put(`${baseURL}/admin/staff/${id}`,payload),
-    //   deleteStaff: (id) => api.delete(`${baseURL}/admin/staff/${id}`)
-
+    updateOrder: (payload) => api.post(`${baseURL}/order/updateOrder`, payload),
+    deleteOrder: (id) => api.delete(`${baseURL}/order/deleteOrder/${id}`)
 }
